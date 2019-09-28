99e48c1660f6133c850dd067fce28a60b69cb56d
Сергей Белан <sbelan@metaprime.ru>
2019-09-29 02:33:44 +0300

    a

--- src/server.js
+++ src/server.js
@@ -13,7 +13,8 @@ const passport = require("koa-passport");
 app = new Koa();
 
 mongoose.set('useCreateIndex', true);
-mongoose.connect('mongodb+srv://mongodb:mongodb@cluster0-yhow8.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }).then((res) => {
+//'mongodb+srv://mongodb:mongodb@cluster0-yhow8.mongodb.net/test?retryWrites=true&w=majority'
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then((res) => {
     console.log('Connection is successful');
 }).catch((e) => {
     throw new Error(e);
@@ -31,6 +32,6 @@ app
   .use(router.routes())
   .use(router.allowedMethods());
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port: ${process.env.PORT}`);
 });
