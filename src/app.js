8232f0cf14c9498b0c57f538805451ae09a881a6
Сергей Белан <sbelan@metaprime.ru>
2019-09-29 01:46:54 +0300

    port

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const bodyParser = require("koa-bodyparser");
 
 const router = require("./routes");
 const mongoose = require("mongoose");
-const PORT = 443;
+const PORT = 3000;
 
 // authentication
 require("./utils/auth");
