71e8b4162d5413a11a604acb51cbbe5ea637dab6
Aksimka <msafonov@metaprime.ru>
2019-09-29 11:15:43 +0300

    404

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app = new Koa();
 
 mongoose.set('useCreateIndex', true);
 //'mongodb+srv://mongodb:mongodb@cluster0-yhow8.mongodb.net/test?retryWrites=true&w=majority'
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then((res) => {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mongodb:mongodb@cluster0-yhow8.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }).then((res) => {
     console.log('Connection is successful');
 }).catch((e) => {
     throw new Error(e);
@@ -28,19 +28,20 @@ const session = require("koa-session");
 app.keys = [process.env.SESSION_SECRET];
 
 // app.use(cors({origin: "*", allowMethods: "*"}));
-app.use(function (ctx, next) {
-    ctx.set('Access-Control-Allow-Origin', '*');
-    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept-Type');
-    ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-    ctx.set('Access-Control-Allow-Credentials', 'true');
-    next();
-});
+
 app.use(session({}, app));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
+app.use(function (ctx, next) {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept-Type');
+    ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    ctx.set('Access-Control-Allow-Credentials', '');
+    next();
+});
 
 
 app.listen(process.env.PORT, () => {
